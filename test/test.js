e5844504778b1b501dbb8525cc08e79567b11c33
Kurt Pattyn <pattyn.kurt@gmail.com>
2015-08-03 17:34:02 +0200

    Add test script and coverage analysis
    Also fixed two bugs detected during test run

---
+++ test/test.js
@@ -0,0 +1,139 @@
+var karl = require('..');
+//karl.setOptions({ redirectConsole: false });
+var assert = require('assert');
+var util = require('util');
+
+var stdout = process.stdout;
+var oldWriteFunc = stdout.write;
+
+describe('karl', function() {
+  var stack = [];
+
+  beforeEach(function() {
+      stack = [];
+      // hook into stdout to see what's being sent to it
+      process.stdout.write = (function(write) {
+        return function(buf, encoding, fd) {
+          //write.call(stdout, buf, encoding, fd);
+          stack.push(buf); // our extra
+        };
+       }(oldWriteFunc));
+  });
+
+  afterEach(function() {
+    process.stdout.write = oldWriteFunc;
+    karl.setOptions({ redirectConsole: false });
+  });
+
+  describe('.info()', function () {
+    it('should send a message to stdout', function (done) {
+      karl.info('test');
+      setImmediate(function() {
+        assert.equal(stack.length, 1);
+        assert(util.isString(stack[0]));
+        var msg = JSON.parse(stack[0]);
+        assert(msg["message"]);
+        assert.equal(msg["message"], 'test');
+        done();
+      });
+    });
+  });
+
+  describe('.debug()', function() {
+    it('should send a message to stdout', function (done) {
+      karl.debug('test');
+      setImmediate(function() {
+        assert.equal(stack.length, 1);
+        assert(util.isString(stack[0]));
+        var msg = JSON.parse(stack[0]);
+        assert(msg["message"]);
+        assert.equal(msg["message"], 'test');
+        done();
+      });
+    });
+  });
+
+  describe('setColorOption', function() {
+    it('should output in color', function(done) {
+      karl.setOptions({ colorize: true, redirectConsole: false });
+      karl.warn('Should be in yellow.');
+      setImmediate(function() {
+        assert.equal(stack.length, 1);
+        assert(util.isString(stack[0]));
+        assert.equal(stack[0].indexOf('\u001b[' + util.inspect.colors.yellow[0]), 0);
+        var sentinel = '\u001b[' + util.inspect.colors.yellow[1];
+        assert.equal(stack[0].indexOf(sentinel), stack[0].length - sentinel.length - 1);
+        done();
+      });
+    });
+
+    it('should not output in color', function(done) {
+      karl.setOptions({ colorize: true, redirectConsole: false });
+      karl.info('Should be in yellow.');
+      setImmediate(function() {
+        assert.equal(stack.length, 1);
+        assert(util.isString(stack[0]));
+        assert.equal(stack[0].indexOf('\u001b['), -1);
+        done();
+      });
+    });
+  });
+
+  describe('setLocationOption', function() {
+    it('should not contain valid location information', function(done) {
+      karl.setOptions({ redirectConsole: false, includeLocationInformation: false });
+      karl.warn('Should have no location information.');
+      setImmediate(function() {
+        assert.equal(stack.length, 1);
+        assert(util.isString(stack[0]));
+        var msg = JSON.parse(stack[0]);
+        assert(util.isObject(msg));
+        assert(msg.hasOwnProperty("fileName"));
+        assert(msg.hasOwnProperty("lineNumber"));
+        assert(msg.hasOwnProperty("functionName"));
+        assert(!msg.fileName);
+        assert(!msg.lineNumber);
+        assert.equal(msg.functionName, "<anonymous>");
+        done();
+      });
+    });
+  });
+
+  describe('humanReadable option on', function() {
+    it('should print multiple lines of json', function(done) {
+      karl.setOptions({ redirectConsole: false, humanReadable: true });
+      karl.info('This should be pretty JSON');
+      setImmediate(function() {
+        assert.equal(stack.length, 1);
+        assert(util.isString(stack[0]));
+        var lines = stack[0].split('\n');
+        assert(lines.length > 1);
+        done();
+      });
+    });
+
+    it('should print a single line with location information', function(done) {
+      karl.setOptions({ redirectConsole: false, humanReadable: true, json: false });
+      karl.warn('Plain ol\'text.');
+      setImmediate(function() {
+        assert.equal(stack.length, 1);
+        assert(util.isString(stack[0]));
+        assert.notEqual(stack[0].indexOf("[WARN]"), -1);
+        assert.notEqual(stack[0].indexOf("test.js"), -1);
+        done();
+      });
+    });
+
+    it('should print a single line without location information', function(done) {
+      karl.setOptions({ redirectConsole: false, humanReadable: true, json: false, includeLocationInformation: false });
+      karl.warn('Plain ol\'text.');
+      setImmediate(function() {
+        assert.equal(stack.length, 1);
+        assert(util.isString(stack[0]));
+        assert.notEqual(stack[0].indexOf("[WARN]"), -1);
+        assert.equal(stack[0].indexOf("test.js"), -1);
+        done();
+      });
+    });
+  });
+});
